e968efcbd8f6a85694bcb16ebee387103fa10e30
Hakim <ozil.hakim@gmail.com>
2022-01-22 10:04:12 -0500

    Updates

---
+++ precache-manifest.9f2281c21731c886207232a7a042766a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26d0bc44dbaec2443c33854125894b5",
+    "url": "/coding-magazine-blog/index.html"
+  },
+  {
+    "revision": "d81adbad17be834536bb",
+    "url": "/coding-magazine-blog/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "a8818f1d48ad091e4edd",
+    "url": "/coding-magazine-blog/static/js/2.bb5ffcce.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/coding-magazine-blog/static/js/2.bb5ffcce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d81adbad17be834536bb",
+    "url": "/coding-magazine-blog/static/js/main.579b1076.chunk.js"
+  },
+  {
+    "revision": "dc646918135e1d9c96ff",
+    "url": "/coding-magazine-blog/static/js/runtime-main.6393d8b3.js"
+  }
+]);
